0d9f4947973c0f2e7ff31110bb6681c38a96a664
Dileepjatav <58458243+Dileepjatav@users.noreply.github.com>
2022-06-12 15:42:21 +0530

    Finial Submissions

--- js/index.js
+++ js/index.js
@@ -1,4 +1,18 @@
 
+let email=document.getElementById('email');
+let val
 
+function print(){
+    val=email.value;
+}
 
-
+    
+  
+    
+function mail(){
+    if (val!=undefined){
+        alert(`Thank you for Subscribe ${val}`)
+        val="";
+        email.value=""
+    }
+}
